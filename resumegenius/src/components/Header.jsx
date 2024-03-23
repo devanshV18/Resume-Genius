5323f60d1f607f7e97f03ac3d7b55ccca323cb5c
devanshV18 <devanshverma024@gmail.com>
2024-03-23 21:02:09 +0530

    login logout functionality done right with home header

--- resumegenius/src/components/Header.jsx
+++ resumegenius/src/components/Header.jsx
@@ -5,10 +5,24 @@ import {Logo} from "../assets"
 import {AnimatePresence,motion} from "framer-motion"
 import { PuffLoader } from 'react-spinners'
 import { RiLogoutCircleLine } from "react-icons/ri";
+import { fadeInOutWithOpacity, slideUpDownMenu } from '../animations'
+import { useQueryClient } from 'react-query'
+import { auth } from '../config/firebase.config'
 
 const Header = () => {
     const {data, isLoading, isError} = useUser();
     const [isMenu,setIsMenu] = useState(false)
+
+    const queryClient = useQueryClient()
+
+    const signOutUser = async() => {
+      await auth.signOut().then(() => {
+        queryClient.setQueryData("user",null);
+      })
+    }
+
+
+
   return (
     <header className='w-full flex items-center justify-between px-4 py-3 lg:px-8 border-b border-gray-300 bg-bgPrimary z-50 gap-12 sticky top-0'> 
 
@@ -37,7 +51,8 @@ const Header = () => {
          (<React.Fragment>
 
           {data ? 
-            (<motion.div className='relative cursor-pointer' onClick={() => setIsMenu(!isMenu)}>
+            (
+            <motion.div className='relative cursor-pointer' onClick={() => setIsMenu(!isMenu)}>
               
               {data?.photoURL ? 
               
@@ -59,10 +74,8 @@ const Header = () => {
           <AnimatePresence>
               
                 {isMenu && 
-                <motion.div 
-                initial={{opacity:0,y:20}}
-                animate={{opacity:1,y:0}}
-                exit={{opacity:0,y:20}}
+                <motion.div {...fadeInOutWithOpacity}
+                {...slideUpDownMenu}
                 className='absolute px-4 py-3 rounded-md bg-white right-0 top-10 flex flex-col items-center justify-start gap-3 w-64 pt-12' onMouseLeave={() => setIsMenu(false)}>
                 
                 {data?.photoURL ? 
@@ -106,7 +119,7 @@ const Header = () => {
                       Add New Template
                     </Link>
   
-                    <div className='w-full px-2 py-2 border-t border-gray-300 flex items-center justify-between group cursor-pointer'>
+                    <div className='w-full px-2 py-2 border-t border-gray-300 flex items-center justify-between group cursor-pointer' onClick={signOutUser}>
                       <p>SignOut</p>
                       
                       <RiLogoutCircleLine className='group-hover:text-txtDark text-txtLight'/>
@@ -124,7 +137,12 @@ const Header = () => {
             ):
 
             (<Link to={"/auth"}>
-              <motion.button>Login</motion.button>
+              <motion.button 
+              className="px-4 py-2 rounded-md border border-indigo-200 hover:shadow-md active:scale-95 duration-150"
+              type='button'
+              {...fadeInOutWithOpacity}>
+                Login
+              </motion.button>
             </Link> ) 
 
           }
