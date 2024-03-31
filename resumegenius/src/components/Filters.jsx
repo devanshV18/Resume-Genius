d344a5f34b589ab22c19497f8211c0c3a8324638
devanshV18 <devanshverma024@gmail.com>
2024-03-31 19:39:33 +0530

    filter section data loaded on ui

--- resumegenius/src/components/Filters.jsx
+++ resumegenius/src/components/Filters.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import {MdLayersClear} from "react-icons/md"
 import { AnimatePresence, motion } from 'framer-motion';
 import { slideDownWithScale } from '../animations';
+import { FiltersData } from '../utils/helpers';
 
 const Filters = () => {
     const [isHover, setIsHover] = useState(false);
@@ -24,6 +25,17 @@ const Filters = () => {
                 </motion.div>
             )}
         </AnimatePresence>
+        </div>
+
+        <div className='w-full flex items-center justify-start overflow-x-scroll gap-6 scrollbar-none'>
+            {FiltersData.map((item) => (
+                <div
+                key={item.id}
+                className={`border bg-black border-white rounded-xl px-6 py-2 cursor-pointer group hover:shadow-md group hover:bg-blue-700`}
+                >
+                    <p className='text-xl text-white whitespace-nowrap'>{item.label}</p>
+                </div>
+            ))}
         </div>
         
 
