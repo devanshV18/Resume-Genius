e69438e64838a4361dfe23665aecdd61b3e0cb72
devanshV18 <devanshverma024@gmail.com>
2024-04-05 23:54:31 +0530

    animation css done for fav and collection button

--- resumegenius/src/components/TemplateDesignPin.jsx
+++ resumegenius/src/components/TemplateDesignPin.jsx
@@ -26,7 +26,7 @@ const TemplateDesignPin = ({data, index}) => {
           {...fadeInOutWithOpacity}
           className='absolute inset-0  flex flex-col items-center justify-start px-4 py-3 z-50 cursor-pointer'
           >
-            <div className='flex flex-col items-end justify-start w-full gap-8 mt-4'>
+            <div className='flex flex-col items-end justify-start w-full gap-8 m-4'>
                 <InnerBoxCard label={"Add to Collection"} Icon={BiFolderPlus} onHandle={addToCollection}/>
                 <InnerBoxCard label={"Add to Favourites"} Icon={BiHeart} onHandle={addToFavourites}/>
             </div>
@@ -47,9 +47,12 @@ const InnerBoxCard = ({label, Icon, onHandle}) => {
 
           <AnimatePresence>
             <motion.div
-            className='px-3 py-2 rounded-md bg-indigo-400 absolute'
+            initial = {{opacity:0, scale:0.6, x:50}}
+            animate = {{opacity:1, scale:1, x:0}}
+            exit = {{opacity:0, scale:0.6, x:50}}
+            className='px-3 py-2 rounded-md bg-indigo-400 absolute -left-44 after:w-2 after:h-2 after:bg-indigi-300 after:absolute after:right-1 after:top-[14px] after:rotate-45'
             >
-             <p className=''>{label}</p>   
+             <p className='text-sm text-black whitespace-nowrap'>{label}</p>   
             </motion.div>
           </AnimatePresence>
         </div>
