56257f1fb29ce78ca0913887abd85078ac9169f5
devanshV18 <devanshverma024@gmail.com>
2024-04-05 20:57:37 +0530

    animatiuon added to loading stuff

--- resumegenius/src/animations/index.js
+++ resumegenius/src/animations/index.js
@@ -17,3 +17,12 @@ export const slideDownWithScale = {
     animate :{opacity:1, scale:1, y:0},
     exit: {opacity:0, scale:0.6, y:20}
 }
+
+export const scaleInOut = (index) => {
+    return {
+    initial: {opacity:0, scale:0.85},
+    animate: {opacity:1, scale:1},
+    exit: {opacity:0, scale:0.85},
+    transition: {delay : index*0.3}
+    }
+}
--- resumegenius/src/components/TemplateDesignPin.jsx
+++ resumegenius/src/components/TemplateDesignPin.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import {motion} from "framer-motion"
+import { scaleInOut } from '../animations'
 
 const TemplateDesignPin = ({data, index}) => {
   return (
     <motion.div 
-    initial = {{opacity:0, scale:0.85}}
-    animate = {{opacity:1, scale:1}}
-    exit = {{opacity:0, scale:0.85}}
-    transition={{delay : index*0.3}}
+    {...scaleInOut}
     >
      <div className='w-full h-[500px] 2xl:h-[740px] rounded-md bg-gray-200 overflow-hidden relative'>
         <img src={data?.imageURL} className='w-full h-full object-cover' alt="" />
